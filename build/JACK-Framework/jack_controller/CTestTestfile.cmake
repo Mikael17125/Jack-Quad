fa2e00f3728cdbbca5cf515349ec7bc005d87060
Mikael Nias <mikael17125@gmail.com>
2020-05-11 18:28:18 +0700

    Adding IMU

--- build/JACK-Framework/jack_controller/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mikael/jack_ws/src/JACK-Framework/jack_controller
-# Build directory: /home/mikael/jack_ws/src/build/JACK-Framework/jack_controller
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/JACK-Motion/bezier_walk/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mikael/jack_ws/src/JACK-Motion/bezier_walk
-# Build directory: /home/mikael/jack_ws/src/build/JACK-Motion/bezier_walk
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
